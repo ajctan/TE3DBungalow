4cfb7b7da975aedfe1c3c94f585491058abe9978
ajctan <aron_tan@dlsu.edu.ph>
2017-12-02 09:11:09 +0800

    searching from profile

--- html/profile.php
+++ html/profile.php
@@ -19,9 +19,9 @@
     </div>
 
     <div id="searchBar">
-      <form action="search.html" method="GET">
-        <input id="searchTerm" type="text" placeholder="Search"/>
-        <button id="searchButton" type="submit">
+      <form action="search.php" method="POST">
+        <input id="searchTerm" type="text" name="search-field" placeholder="Search"/>
+        <button id="searchButton" type="submit" name="search-button">
           <i class="fa fa-search"></i>
         </button>
       </form>
