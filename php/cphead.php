2dcae8410f7208728965fb9af60a8d1271b1ef75
ajctan <aron_tan@dlsu.edu.ph>
2017-12-06 18:22:13 +0800

    almost done fixing email

--- php/cphead.php
+++ php/cphead.php
@@ -1,13 +1,14 @@
 <?php
+	include '../php/dbh.php';
 	$email = $_POST['email'];
 	$msg = $_POST['message'];
 
 	$sql = "SELECT * FROM tptable WHERE tpID LIKE ".$_POST['projID'];
     $result = mysqli_query($conn,$sql);
     $row = mysqli_fetch_assoc($result);
-    
 
 
-	$result = mail("projectHead@te3dhouse.com", "[TE3DBungalow] Notification", "[".$email." on ".$row['tpTitle']."]: ".$msg, "From: te3dAdmin@gmail.com");
+    $result = mail("projectHead@te3dhouse.com", "[TE3DBungalow] Notification", "[".$email." on ".$row['tpTitle']."]: ".$msg, "From: te3dAdmin@gmail.com");
+	//$result = mail("projectHead@te3dhouse.com", "[TE3DBungalow] Notification", "[".$email." on ".$row['tpTitle']."]: ".$msg, "From: te3dAdmin@gmail.com");
 	header('Location: ../html/project.php');
 ?>
