2b50f5a45d3cea07f5db036c6d784c305e3ced20
ajctan <aron_tan@dlsu.edu.ph>
2017-12-06 21:54:34 +0800

    done: delete project

--- php/deleteProject.php
+++ php/deleteProject.php
@@ -1,3 +1,11 @@
 <?php
-	
+	include '../php/dbh.php';
+	$pid = $_POST['dlete'];
+
+	$sql = "DELETE FROM files WHERE tpID LIKE ".$pid;
+    $result = mysqli_query($conn,$sql);
+    $sql = "DELETE FROM tpTable WHERE tpID LIKE ".$pid;
+    $result = mysqli_query($conn,$sql);
+    unset($_COOKIE['PID']);
+    header('Location: ../html/index.php');
 ?>
