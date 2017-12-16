9ce604d60d0c57fa6b53f9049abce8f0ccfb397d
Jesus Castro <joesei_castro@dlsu.edu.ph>
2017-12-16 20:53:33 +0800

    Deleting a Project Deletes Files Associated With It

--- php/deleteProject.php
+++ php/deleteProject.php
@@ -2,6 +2,25 @@
 	include '../php/dbh.php';
 	$pid = $_POST['delete'];
 
+	if (file_exists('../projectFiles/'.$pid)) {
+		delete_files('../projectFiles/'.$pid);
+	}
+
+	function delete_files($target) {
+			if(is_dir($target)){
+					$files = glob( $target . '*', GLOB_MARK );
+
+					foreach( $files as $file )
+					{
+							delete_files( $file );
+					}
+
+					rmdir( $target );
+			} elseif(is_file($target)) {
+					unlink( $target );
+			}
+	}
+
 	$sql = 'DELETE FROM files WHERE tpID = '.$pid;
   $result = mysqli_query($conn,$sql);
 	$sql = 'DELETE FROM tpTable WHERE tpID = '.$pid;
